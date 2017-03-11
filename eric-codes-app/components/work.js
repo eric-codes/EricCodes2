9398512997ee8a8bb6670d16d3924dfcaf341296
Eric Cheung <eric@flexmg.com>
2017-03-10 19:10:06 -0500

    v1.1.1-31 [UPDATE] - added some work stuff

--- eric-codes-app/components/work.js
+++ eric-codes-app/components/work.js
@@ -1,4 +1,145 @@
+app.controller('work', ['$scope', '$rootScope', function($scope, $rootScope) {
 
-app.controller('work', ['$scope','$rootScope', function($scope,$rootScope){
-	
+    $scope.WorkItems = [{
+        name: "Chisel Cartel",
+        codeName: "chisel.cartel",
+        blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero.",
+        tags: [{
+            icon: iconFolder + "code_jquery.svg",
+            title: "jQuery"
+        }, {
+            icon: iconFolder + "code_angular.svg",
+            title: "Angular 1.x"
+        }, {
+            icon: iconFolder + "code_sass.svg",
+            title: "SASS / LESS"
+        }, {
+            icon: iconFolder + "code_backend.svg",
+            title: "PHP / MySQL"
+        }, {
+            icon: iconFolder + "code_css3.svg",
+            title: "CSS3"
+        }, {
+            icon: iconFolder + "code_graphicdesign.svg",
+            title: "Graphic Design"
+        }, {
+            icon: iconFolder + "code_gulp.svg",
+            title: "Gulp"
+        }, {
+            icon: iconFolder + "code_html5.svg",
+            title: "HTML5"
+        }, {
+            icon: iconFolder + "code_typography.svg",
+            title: "Typography"
+        }, {
+            icon: iconFolder + "code_uidesign.svg",
+            title: "UI Design"
+        }, ]
+    }, {
+        name: "Chisel Cartel",
+        codeName: "chisel.cartel",
+        blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero.",
+        tags: [{
+            icon: iconFolder + "code_jquery.svg",
+            title: "jQuery"
+        }, {
+            icon: iconFolder + "code_angular.svg",
+            title: "Angular 1.x"
+        }, {
+            icon: iconFolder + "code_sass.svg",
+            title: "SASS / LESS"
+        }, {
+            icon: iconFolder + "code_backend.svg",
+            title: "PHP / MySQL"
+        }, {
+            icon: iconFolder + "code_css3.svg",
+            title: "CSS3"
+        }, {
+            icon: iconFolder + "code_graphicdesign.svg",
+            title: "Graphic Design"
+        }, {
+            icon: iconFolder + "code_gulp.svg",
+            title: "Gulp"
+        }, {
+            icon: iconFolder + "code_html5.svg",
+            title: "HTML5"
+        }, {
+            icon: iconFolder + "code_typography.svg",
+            title: "Typography"
+        }, {
+            icon: iconFolder + "code_uidesign.svg",
+            title: "UI Design"
+        }, ]
+    }, {
+        name: "Chisel Cartel",
+        codeName: "chisel.cartel",
+        blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero.",
+        tags: [{
+            icon: iconFolder + "code_jquery.svg",
+            title: "jQuery"
+        }, {
+            icon: iconFolder + "code_angular.svg",
+            title: "Angular 1.x"
+        }, {
+            icon: iconFolder + "code_sass.svg",
+            title: "SASS / LESS"
+        }, {
+            icon: iconFolder + "code_backend.svg",
+            title: "PHP / MySQL"
+        }, {
+            icon: iconFolder + "code_css3.svg",
+            title: "CSS3"
+        }, {
+            icon: iconFolder + "code_graphicdesign.svg",
+            title: "Graphic Design"
+        }, {
+            icon: iconFolder + "code_gulp.svg",
+            title: "Gulp"
+        }, {
+            icon: iconFolder + "code_html5.svg",
+            title: "HTML5"
+        }, {
+            icon: iconFolder + "code_typography.svg",
+            title: "Typography"
+        }, {
+            icon: iconFolder + "code_uidesign.svg",
+            title: "UI Design"
+        }, ]
+    }, {
+        name: "Chisel Cartel",
+        codeName: "chisel.cartel",
+        blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero.",
+        tags: [{
+            icon: iconFolder + "code_jquery.svg",
+            title: "jQuery"
+        }, {
+            icon: iconFolder + "code_angular.svg",
+            title: "Angular 1.x"
+        }, {
+            icon: iconFolder + "code_sass.svg",
+            title: "SASS / LESS"
+        }, {
+            icon: iconFolder + "code_backend.svg",
+            title: "PHP / MySQL"
+        }, {
+            icon: iconFolder + "code_css3.svg",
+            title: "CSS3"
+        }, {
+            icon: iconFolder + "code_graphicdesign.svg",
+            title: "Graphic Design"
+        }, {
+            icon: iconFolder + "code_gulp.svg",
+            title: "Gulp"
+        }, {
+            icon: iconFolder + "code_html5.svg",
+            title: "HTML5"
+        }, {
+            icon: iconFolder + "code_typography.svg",
+            title: "Typography"
+        }, {
+            icon: iconFolder + "code_uidesign.svg",
+            title: "UI Design"
+        }, ]
+    }, ]
+
 }])
