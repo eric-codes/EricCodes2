a93911a341261c5c1a30a50a9e5661c8cb2e62b4
Eric Cheung <eric@flexmg.com>
2017-03-13 16:24:03 -0400

    v1.2.1-28 [UPDATE] - added some extra work-single page stuff

--- eric-codes-app/components/work_single.js
+++ eric-codes-app/components/work_single.js
@@ -2,6 +2,20 @@ app.controller('work_single', ['$scope', '$rootScope', function($scope, $rootSco
 
     $rootScope.BodyClass = "work-single";
 
+    $scope.AllProjects = [{
+        name: "Chisel Cartel",
+        slug: "chisel-cartel"
+    }, {
+        name: "Chisel Cartel 2",
+        slug: "chisel-cartel"
+    }, {
+        name: "Chisel Cartel 3",
+        slug: "chisel-cartel"
+    }, {
+        name: "Chisel Cartel 4",
+        slug: "chisel-cartel"
+    }]
+
     $scope.WorkData = {
         title: "chisel.cartel",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero.",
@@ -51,18 +65,16 @@ app.controller('work_single', ['$scope', '$rootScope', function($scope, $rootSco
             'backend',
             'angular',
         ]),
-    	textSections: [
-    	{
-    		title: "frontEnd()",
-    		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero. "
-    	},{
-    		title: "backEnd()",
-    		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero. "
-    	},{
-    		title: "automation()",
-    		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero. "
-    	},
-    	]
+        textSections: [{
+            title: "frontEnd()",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero. "
+        }, {
+            title: "backEnd()",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero. "
+        }, {
+            title: "automation()",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget venenatis lectus. Suspendisse mollis facilisis sapien at rutrum. Nulla scelerisque gravida libero. Duis vestibulum diam a nulla feugiat cursus. Donec luctus, massa eu elementum vulputate, purus lectus lacinia enim, in rutrum purus lacus nec nibh. Proin mollis semper blandit. Phasellus eget enim consectetur, laoreet lorem quis, tincidunt libero. "
+        }, ]
     }, {
         gallery: [{
             URL: "http://placehold.it/640x480"
