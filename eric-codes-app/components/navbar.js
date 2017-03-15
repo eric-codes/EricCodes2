ce09bd518d5c8a8f92805057d55a48fe88d05b9b
Eric Cheung <eric@flexmg.com>
2017-03-15 17:28:06 -0400

    v1.4.1-39 [UPDATE] - -- push to FTP --

--- eric-codes-app/components/navbar.js
+++ eric-codes-app/components/navbar.js
@@ -7,7 +7,7 @@ app.controller('navbar', ['$scope', '$rootScope', 'breadcrumbs', function($scope
         link: '/'
     }];
 
-    $rootScope.NavHidden = true;
+    $rootScope.NavHidden = false;
 
     /**
      * $rootScope nav text array.
