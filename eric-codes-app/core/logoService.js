eb2eff849fc3fe2f21e0911f8e0de6106e4c15a0
Eric Cheung <eric@flexmg.com>
2017-03-16 23:21:03 -0400

    v1.4.1-60 [UPDATE] - -- push to FTP --

--- eric-codes-app/core/logoService.js
+++ eric-codes-app/core/logoService.js
@@ -33,14 +33,19 @@ app.factory('breadcrumbs', ['$rootScope', function($rootScope) {
         $rootScope.NavText = currentNav;
     }
 
-    scope.updateSecondChild = function(obj,parent) {
+    scope.updateSecondChild = function(obj, parent) {
         Log.Function('Updating Nav Second Child');
 
         var currentNav = $rootScope.NavText;
 
         Log.Set('currentNav before change', currentNav);
 
+        if (parent[1].text && parent[1].text !== currentNav[1].text) {
             currentNav[1] = parent;
+        }
+
+
+
         currentNav[2] = obj;
 
         Log.Set('currentNav after change', currentNav);
