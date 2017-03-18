753d5e61f52e0d5b780b04e8fef81cf9b007ca76
Eric Cheung <eric@flexmg.com>
2017-03-18 04:55:04 -0400

    v1.6.1-0 [UPDATE] - -- push to FTP --

--- eric-codes-theme/app/app.directives.min.js
+++ eric-codes-theme/app/app.directives.min.js
@@ -1,1 +1,52 @@
-app.directive("socialIcons",function(){function e(e,i,a){e.SocialIcons=[{icon:GetShared("social_linkedin")},{icon:GetShared("social_behance")},{icon:GetShared("social_twitter")}]}return{scope:{maindata:"="},restrict:"E",templateUrl:GetShared("social-icons"),link:e}}),app.directive("workGallery",function(){function e(e,i,a){e.maindata,e.currentSlide=e.maindata[1],e.ChangeSlide=function(i){Log.Msg("Gallery item clicked!"),e.currentSlide=e.maindata[i]}}return{scope:{maindata:"="},restrict:"E",templateUrl:GetShared("work-gallery"),link:e}});
+app.directive('socialIcons', function() {
+
+    function link_return(scope, elem, attr) {
+
+        scope.SocialIcons = [{
+            icon: GetShared("social_linkedin")
+        }, {
+            icon: GetShared("social_behance")
+        }, {
+            icon: GetShared("social_twitter")
+        }];
+
+    }
+
+    // Runs during compile
+    return {
+        scope: {
+            maindata: "="
+        },
+        restrict: 'E',
+        templateUrl: GetShared('social-icons'),
+        link: link_return
+    };
+});
+
+
+
+app.directive('workGallery', function(){
+
+	function link_return(scope,elem,attr){
+
+		scope.maindata;
+
+		scope.currentSlide = scope.maindata[1];
+
+		scope.ChangeSlide = function(i){
+			Log.Msg('Gallery item clicked!');
+			scope.currentSlide = scope.maindata[i];
+		}
+
+	}
+
+	// Runs during compile
+	return {
+		scope: {
+			maindata: "="
+		}, 
+		 restrict: 'E',
+		 templateUrl: GetShared('work-gallery'),
+		 link: link_return
+	};
+});
