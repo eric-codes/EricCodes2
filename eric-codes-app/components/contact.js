a453a5b0e42c353c23fbcfbdc31bb0a60b03f234
Eric Cheung <eric@flexmg.com>
2017-03-18 01:38:17 -0400

    v1.5.0 [UPDATE] - -- push to FTP --

--- eric-codes-app/components/contact.js
+++ eric-codes-app/components/contact.js
@@ -1,5 +1,14 @@
-app.controller('contact', ['$scope', '$rootScope', function($scope,$rootScope){
-	
-	
-	
+app.controller('contact', ['$scope', '$rootScope', 'breadcrumbs', function($scope, $rootScope, breadcrumbs) {
+
+    breadcrumbs.updateNav(2);
+
+    $rootScope.BodyClass = "contact";
+
+    $rootScope.NavHidden = false;
+
+    breadcrumbs.updateFirstChild({
+        text: ".contact()",
+        link: "/contact"
+    })
+
 }])
