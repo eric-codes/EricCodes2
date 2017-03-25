c7b15f599c2246b53ea297c88a5f2ba9afbff23b
Eric Cheung <eric@flexmg.com>
2017-03-24 22:25:13 -0400

    v1.8.1-14 [UPDATE] - added code-viewer directive

---
+++ eric-codes-app/shared/code-viewer.js
@@ -0,0 +1,24 @@
+app.directive('codeViewer', ['$rootScope', function($rootScope){
+
+
+	function link_return(scope,elem,attr) {
+
+		var scope = {};
+
+		scope.maindata;
+
+		
+
+	}
+
+
+    // Runs during compile
+    return {
+        scope: {
+            maindata: "="
+        },
+        restrict: 'E',
+        templateUrl: GetShared('code-viewer'),
+        link: link_return
+    };
+}]);
