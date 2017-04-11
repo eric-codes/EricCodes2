303be56e7cb51ce6c7d2e4e0148e20316a0b9314
Eric Cheung <eric@flexmg.com>
2017-04-10 23:38:03 -0400

    v1.9.1 [UPDATE] - -- push to FTP --

--- eric-codes-app/core/core-controller.js
+++ eric-codes-app/core/core-controller.js
@@ -10,31 +10,42 @@ app.controller('CoreController', ['$scope', '$rootScope', 'loadData', function($
 
     loadData.GetData().then(function(data) {
 
-    	var FileSet = data.data;
+        var FileSet = data.data;
 
         Log.Set('FileSet', FileSet);
 
         var AllFiles = {};
         var WorkItems = [];
 
-        $.each(FileSet,function(i,val){
-        	AllFiles[val.workData.slug] = val;
-        	WorkItems.push(val.workData);
+        $.each(FileSet, function(i, val) {
+            AllFiles[val.workData.slug] = val;
+            WorkItems.push(val.workData);
         })
 
 
         $rootScope.AllData = AllFiles;
         $rootScope.WorkItems = WorkItems;
 
-        Log.Set("$rootScope.AllData",$rootScope.AllData);
-        Log.Set("$rootScope.WorkItems",$rootScope.WorkItems);
+        Log.Set("$rootScope.AllData", $rootScope.AllData);
+        Log.Set("$rootScope.WorkItems", $rootScope.WorkItems);
 
         var PreloadList = [];
 
-        $.each(AllFiles,function(i,val){
+        $.each(AllFiles, function(i, val) {
             var Return = themeURL + "assets/img/" + i + "/background.jpg";
-            Log.Set('Preload for '+i,Return);
+            Log.Set('Preload for ' + i, Return);
             PreloadList.push(Return);
+
+            $.each(val.sections, function(i2, val2) {
+                if (val2.gallery) {
+                    $.each(val2.gallery, function(itemIndex, item) {
+                        var Return = themeURL + "assets/img/" + item.URL,
+                            ReturnThumb = themeURL + "assets/img/" + item.thumbnail;
+                            PreloadList.push(Return,ReturnThumb);
+                    })
+                }
+            })
+
         })
 
         preload(PreloadList);
