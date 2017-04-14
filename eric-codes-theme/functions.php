85a06fbcbb10f0d77f89cceeb52056be1668d5f4
Eric Cheung <eric@flexmg.com>
2017-04-14 00:40:08 -0400

    v1.9.2-1 [UPDATE] - -- push to FTP --

--- eric-codes-theme/functions.php
+++ eric-codes-theme/functions.php
@@ -9,7 +9,7 @@ function DispatchMail($args) {
 add_action( 'rest_api_init', function(){
 	register_rest_route('ericcodes/v1','/send',array(
 		'methods' => 'POST',
-		'callback' = 'DispatchMail'
+		'callback' => 'DispatchMail'
 		));
 });
 
