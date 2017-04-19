b5014e5d51ce41c2fc8cceea7c5c288a0ac690b0
Eric Cheung <eric@flexmg.com>
2017-04-19 18:49:35 -0400

    v1.9.2 [UPDATE] - -- push to FTP --

--- eric-codes-theme/index.php
+++ eric-codes-theme/index.php
@@ -73,9 +73,9 @@ function TemplateGet($input) {
 
 				<div class="pull-left logo-area">
 					<span class="logo-type">
-						<a ng-href="{{crumb[0].link}}" class="crumb-0"></a>
-						<a ng-href="{{crumb[1].link}}" class="crumb-1"></a>
-						<a ng-href="{{crumb[2].link}}" class="crumb-2"></a>
+						<a ng-href="{{crumb[0].link}}" class="crumb crumb-0"></a>
+						<a ng-href="{{crumb[1].link}}" class="crumb crumb-1"></a>
+						<a ng-href="{{crumb[2].link}}" class="crumb crumb-2"></a>
 					</span>
 				</div>
 
