d5c0b4c47e9f0b0318fe6a845c19f9dbcdc3c959
Eric Cheung <eric@flexmg.com>
2017-04-19 18:36:23 -0400

    v1.9.2-10 [PATCH] - RELEASE TO PUBLIC

--- eric-codes-theme/app/components/navbar.js
+++ eric-codes-theme/app/components/navbar.js
@@ -170,7 +170,7 @@ app.controller('navbar', ['$scope', '$rootScope', 'breadcrumbs', function($scope
             }
 
             if ($('.crumb:eq(0)').text().length < 1) {
-            	Animate.In(newv[0].text, 0);
+                Animate.In(newv[0].text, 0);
             }
 
         } else if (!newv[1]) {
@@ -257,12 +257,12 @@ app.controller('navbar', ['$scope', '$rootScope', 'breadcrumbs', function($scope
     }, true);
 
     $rootScope.$watch('NavHidden', function(newValue, oldValue) {
-    	
-    	if (newValue === true) {
-    		$('.navbar').addClass('not-shown');
-    	} else {
-    		$('.navbar').removeClass('not-shown');
-    	}
+
+        if (newValue === true) {
+            $('.navbar').addClass('not-shown');
+        } else {
+            $('.navbar').removeClass('not-shown');
+        }
 
     }, true);
 
